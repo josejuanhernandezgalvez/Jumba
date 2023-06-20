c7689724e295f229dc844b77af3a4f1eb2c2b943
Octavio Roncal <octavioroncal11@gmail.com>
2023-06-20 11:31:27 +0100

    wip

--- blatt/gen/io/flogo/blatt/model/Tuning.java
+++ blatt/gen/io/flogo/blatt/model/Tuning.java
@@ -3,31 +3,55 @@ package io.flogo.blatt.model;
 import io.flogo.blatt.model.*;
 
 public class Tuning  extends io.intino.magritte.framework.Layer implements io.intino.magritte.framework.tags.Terminal {
-	protected java.util.List<io.flogo.blatt.model.Tuning.Optimization> optimizationList = new java.util.ArrayList<>();
+	protected io.flogo.blatt.model.Tuning.Optimize optimize;
+	protected java.util.List<io.flogo.blatt.model.Tuning.BestOptimizer> bestOptimizerList = new java.util.ArrayList<>();
+	protected java.util.List<io.flogo.blatt.model.Tuning.BestLossFunction> bestLossFunctionList = new java.util.ArrayList<>();
 
 	public Tuning(io.intino.magritte.framework.Node node) {
 		super(node);
 	}
 
-	public java.util.List<io.flogo.blatt.model.Tuning.Optimization> optimizationList() {
-		return java.util.Collections.unmodifiableList(optimizationList);
+	public io.flogo.blatt.model.Tuning.Optimize optimize() {
+		return optimize;
 	}
 
-	public io.flogo.blatt.model.Tuning.Optimization optimization(int index) {
-		return optimizationList.get(index);
+	public java.util.List<io.flogo.blatt.model.Tuning.BestOptimizer> bestOptimizerList() {
+		return java.util.Collections.unmodifiableList(bestOptimizerList);
 	}
 
-	public java.util.List<io.flogo.blatt.model.Tuning.Optimization> optimizationList(java.util.function.Predicate<io.flogo.blatt.model.Tuning.Optimization> predicate) {
-		return optimizationList().stream().filter(predicate).collect(java.util.stream.Collectors.toList());
+	public io.flogo.blatt.model.Tuning.BestOptimizer bestOptimizer(int index) {
+		return bestOptimizerList.get(index);
 	}
 
-	public io.flogo.blatt.model.Tuning.Optimization optimization(java.util.function.Predicate<io.flogo.blatt.model.Tuning.Optimization> predicate) {
-		return optimizationList().stream().filter(predicate).findFirst().orElse(null);
+	public java.util.List<io.flogo.blatt.model.Tuning.BestOptimizer> bestOptimizerList(java.util.function.Predicate<io.flogo.blatt.model.Tuning.BestOptimizer> predicate) {
+		return bestOptimizerList().stream().filter(predicate).collect(java.util.stream.Collectors.toList());
+	}
+
+	public io.flogo.blatt.model.Tuning.BestOptimizer bestOptimizer(java.util.function.Predicate<io.flogo.blatt.model.Tuning.BestOptimizer> predicate) {
+		return bestOptimizerList().stream().filter(predicate).findFirst().orElse(null);
+	}
+
+	public java.util.List<io.flogo.blatt.model.Tuning.BestLossFunction> bestLossFunctionList() {
+		return java.util.Collections.unmodifiableList(bestLossFunctionList);
+	}
+
+	public io.flogo.blatt.model.Tuning.BestLossFunction bestLossFunction(int index) {
+		return bestLossFunctionList.get(index);
+	}
+
+	public java.util.List<io.flogo.blatt.model.Tuning.BestLossFunction> bestLossFunctionList(java.util.function.Predicate<io.flogo.blatt.model.Tuning.BestLossFunction> predicate) {
+		return bestLossFunctionList().stream().filter(predicate).collect(java.util.stream.Collectors.toList());
+	}
+
+	public io.flogo.blatt.model.Tuning.BestLossFunction bestLossFunction(java.util.function.Predicate<io.flogo.blatt.model.Tuning.BestLossFunction> predicate) {
+		return bestLossFunctionList().stream().filter(predicate).findFirst().orElse(null);
 	}
 
 	protected java.util.List<io.intino.magritte.framework.Node> componentList$() {
 		java.util.Set<io.intino.magritte.framework.Node> components = new java.util.LinkedHashSet<>(super.componentList$());
-		new java.util.ArrayList<>(optimizationList).forEach(c -> components.add(c.core$()));
+		if (optimize != null) components.add(this.optimize.core$());
+		new java.util.ArrayList<>(bestOptimizerList).forEach(c -> components.add(c.core$()));
+		new java.util.ArrayList<>(bestLossFunctionList).forEach(c -> components.add(c.core$()));
 		return new java.util.ArrayList<>(components);
 	}
 
@@ -40,13 +64,17 @@ public class Tuning  extends io.intino.magritte.framework.Layer implements io.in
 	@Override
 	protected void addNode$(io.intino.magritte.framework.Node node) {
 		super.addNode$(node);
-		if (node.is("Tuning$Optimization")) this.optimizationList.add(node.as(io.flogo.blatt.model.Tuning.Optimization.class));
+		if (node.is("Tuning$Optimize")) this.optimize = node.as(io.flogo.blatt.model.Tuning.Optimize.class);
+		if (node.is("Tuning$BestOptimizer")) this.bestOptimizerList.add(node.as(io.flogo.blatt.model.Tuning.BestOptimizer.class));
+		if (node.is("Tuning$BestLossFunction")) this.bestLossFunctionList.add(node.as(io.flogo.blatt.model.Tuning.BestLossFunction.class));
 	}
 
 	@Override
 	protected void removeNode$(io.intino.magritte.framework.Node node) {
 		super.removeNode$(node);
-		if (node.is("Tuning$Optimization")) this.optimizationList.remove(node.as(io.flogo.blatt.model.Tuning.Optimization.class));
+		if (node.is("Tuning$Optimize")) this.optimize = null;
+		if (node.is("Tuning$BestOptimizer")) this.bestOptimizerList.remove(node.as(io.flogo.blatt.model.Tuning.BestOptimizer.class));
+		if (node.is("Tuning$BestLossFunction")) this.bestLossFunctionList.remove(node.as(io.flogo.blatt.model.Tuning.BestLossFunction.class));
 	}
 
 	@Override
@@ -74,9 +102,21 @@ public class Tuning  extends io.intino.magritte.framework.Layer implements io.in
 			this.name = name;
 		}
 
-		public io.flogo.blatt.model.Tuning.Optimization optimization(java.util.List<io.flogo.blatt.model.NeuralNetwork> neuralNetworks) {
-			io.flogo.blatt.model.Tuning.Optimization newElement = core$().graph().concept(io.flogo.blatt.model.Tuning.Optimization.class).createNode(this.name, core$()).as(io.flogo.blatt.model.Tuning.Optimization.class);
+		public io.flogo.blatt.model.Tuning.Optimize optimize(java.util.List<io.flogo.blatt.model.NeuralNetwork> neuralNetworks) {
+			io.flogo.blatt.model.Tuning.Optimize newElement = core$().graph().concept(io.flogo.blatt.model.Tuning.Optimize.class).createNode(this.name, core$()).as(io.flogo.blatt.model.Tuning.Optimize.class);
 			newElement.core$().set(newElement, "neuralNetworks", neuralNetworks);
+			return newElement;
+		}
+
+		public io.flogo.blatt.model.Tuning.BestOptimizer bestOptimizer(io.flogo.blatt.model.NeuralNetwork neuralNetwork) {
+			io.flogo.blatt.model.Tuning.BestOptimizer newElement = core$().graph().concept(io.flogo.blatt.model.Tuning.BestOptimizer.class).createNode(this.name, core$()).as(io.flogo.blatt.model.Tuning.BestOptimizer.class);
+			newElement.core$().set(newElement, "neuralNetwork", java.util.Collections.singletonList(neuralNetwork));
+			return newElement;
+		}
+
+		public io.flogo.blatt.model.Tuning.BestLossFunction bestLossFunction(io.flogo.blatt.model.NeuralNetwork neuralNetwork) {
+			io.flogo.blatt.model.Tuning.BestLossFunction newElement = core$().graph().concept(io.flogo.blatt.model.Tuning.BestLossFunction.class).createNode(this.name, core$()).as(io.flogo.blatt.model.Tuning.BestLossFunction.class);
+			newElement.core$().set(newElement, "neuralNetwork", java.util.Collections.singletonList(neuralNetwork));
 			return newElement;
 		}
 
@@ -87,15 +127,19 @@ public class Tuning  extends io.intino.magritte.framework.Layer implements io.in
 	}
 
 	public class Clear  {
-		public void optimization(java.util.function.Predicate<io.flogo.blatt.model.Tuning.Optimization> filter) {
-			new java.util.ArrayList<>(optimizationList()).stream().filter(filter).forEach(io.intino.magritte.framework.Layer::delete$);
+		public void bestOptimizer(java.util.function.Predicate<io.flogo.blatt.model.Tuning.BestOptimizer> filter) {
+			new java.util.ArrayList<>(bestOptimizerList()).stream().filter(filter).forEach(io.intino.magritte.framework.Layer::delete$);
+		}
+
+		public void bestLossFunction(java.util.function.Predicate<io.flogo.blatt.model.Tuning.BestLossFunction> filter) {
+			new java.util.ArrayList<>(bestLossFunctionList()).stream().filter(filter).forEach(io.intino.magritte.framework.Layer::delete$);
 		}
 	}
 
-	public static class Optimization  extends io.intino.magritte.framework.Layer implements io.intino.magritte.framework.tags.Terminal {
+	public static class Optimize  extends io.intino.magritte.framework.Layer implements io.intino.magritte.framework.tags.Terminal {
 		protected java.util.List<io.flogo.blatt.model.NeuralNetwork> neuralNetworks = new java.util.ArrayList<>();
 
-		public Optimization(io.intino.magritte.framework.Node node) {
+		public Optimize(io.intino.magritte.framework.Node node) {
 			super(node);
 		}
 
@@ -129,6 +173,86 @@ public class Tuning  extends io.intino.magritte.framework.Layer implements io.in
 			super.set$(name, values);
 			if (name.equalsIgnoreCase("neuralNetworks")) this.neuralNetworks = ((java.util.List<java.lang.Object>) values).stream().
 				map(s -> graph().core$().load(((io.intino.magritte.framework.Layer) s).core$().id()).as(io.flogo.blatt.model.NeuralNetwork.class)).collect(java.util.stream.Collectors.toList());
+		}
+
+		public io.flogo.blatt.model.BlattGraph graph() {
+			return (io.flogo.blatt.model.BlattGraph) core$().graph().as(io.flogo.blatt.model.BlattGraph.class);
+		}
+	}
+
+	public static class BestOptimizer  extends io.intino.magritte.framework.Layer implements io.intino.magritte.framework.tags.Terminal {
+		protected io.flogo.blatt.model.NeuralNetwork neuralNetwork;
+
+		public BestOptimizer(io.intino.magritte.framework.Node node) {
+			super(node);
+		}
+
+		public io.flogo.blatt.model.NeuralNetwork neuralNetwork() {
+			return neuralNetwork;
+		}
+
+		public BestOptimizer neuralNetwork(io.flogo.blatt.model.NeuralNetwork value) {
+			this.neuralNetwork = value;
+			return (BestOptimizer) this;
+		}
+
+		@Override
+		protected java.util.Map<java.lang.String, java.util.List<?>> variables$() {
+			java.util.Map<java.lang.String, java.util.List<?>> map = new java.util.LinkedHashMap<>();
+			map.put("neuralNetwork", this.neuralNetwork != null ? new java.util.ArrayList(java.util.Collections.singletonList(this.neuralNetwork)) : java.util.Collections.emptyList());
+			return map;
+		}
+
+		@Override
+		protected void load$(java.lang.String name, java.util.List<?> values) {
+			super.load$(name, values);
+			if (name.equalsIgnoreCase("neuralNetwork")) this.neuralNetwork = io.intino.magritte.framework.loaders.NodeLoader.load(values, io.flogo.blatt.model.NeuralNetwork.class, this).get(0);
+		}
+
+		@Override
+		protected void set$(java.lang.String name, java.util.List<?> values) {
+			super.set$(name, values);
+			if (name.equalsIgnoreCase("neuralNetwork")) this.neuralNetwork = values.get(0)!= null ? core$().graph().load(((io.intino.magritte.framework.Layer) values.get(0)).core$().id()).as(io.flogo.blatt.model.NeuralNetwork.class) : null;
+		}
+
+		public io.flogo.blatt.model.BlattGraph graph() {
+			return (io.flogo.blatt.model.BlattGraph) core$().graph().as(io.flogo.blatt.model.BlattGraph.class);
+		}
+	}
+
+	public static class BestLossFunction  extends io.intino.magritte.framework.Layer implements io.intino.magritte.framework.tags.Terminal {
+		protected io.flogo.blatt.model.NeuralNetwork neuralNetwork;
+
+		public BestLossFunction(io.intino.magritte.framework.Node node) {
+			super(node);
+		}
+
+		public io.flogo.blatt.model.NeuralNetwork neuralNetwork() {
+			return neuralNetwork;
+		}
+
+		public BestLossFunction neuralNetwork(io.flogo.blatt.model.NeuralNetwork value) {
+			this.neuralNetwork = value;
+			return (BestLossFunction) this;
+		}
+
+		@Override
+		protected java.util.Map<java.lang.String, java.util.List<?>> variables$() {
+			java.util.Map<java.lang.String, java.util.List<?>> map = new java.util.LinkedHashMap<>();
+			map.put("neuralNetwork", this.neuralNetwork != null ? new java.util.ArrayList(java.util.Collections.singletonList(this.neuralNetwork)) : java.util.Collections.emptyList());
+			return map;
+		}
+
+		@Override
+		protected void load$(java.lang.String name, java.util.List<?> values) {
+			super.load$(name, values);
+			if (name.equalsIgnoreCase("neuralNetwork")) this.neuralNetwork = io.intino.magritte.framework.loaders.NodeLoader.load(values, io.flogo.blatt.model.NeuralNetwork.class, this).get(0);
+		}
+
+		@Override
+		protected void set$(java.lang.String name, java.util.List<?> values) {
+			super.set$(name, values);
+			if (name.equalsIgnoreCase("neuralNetwork")) this.neuralNetwork = values.get(0)!= null ? core$().graph().load(((io.intino.magritte.framework.Layer) values.get(0)).core$().id()).as(io.flogo.blatt.model.NeuralNetwork.class) : null;
 		}
 
 		public io.flogo.blatt.model.BlattGraph graph() {
--- builder/src/io/flogo/builder/operations/FlogoGenerationOperation.java
+++ builder/src/io/flogo/builder/operations/FlogoGenerationOperation.java
@@ -1,9 +1,16 @@
 package io.flogo.builder.operations;
 
+import io.intino.itrules.Frame;
+import io.intino.itrules.FrameBuilder;
 import io.intino.magritte.builder.core.CompilationUnit;
 import io.intino.magritte.builder.core.errorcollection.CompilationFailedException;
 import io.intino.magritte.builder.core.operation.model.ModelOperation;
 import io.intino.magritte.builder.model.Model;
+import io.intino.magritte.lang.model.Node;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class FlogoGenerationOperation extends ModelOperation {
 
@@ -14,6 +21,25 @@ public class FlogoGenerationOperation extends ModelOperation {
 
 	@Override
 	public void call(Model model) throws CompilationFailedException {
+		FrameBuilder builder = new FrameBuilder();
+		for (Node component : model.components()) {
+			builder.add(component.type())
+					.add("name", component.name())
+					.add("section", component.components().stream()
+							.filter(c -> c.types().contains("Section"))
+							.map(this::sectionFrameOf)
+							.toArray(Frame[]::new));
+		}
+		Frame frame = builder.toFrame();
+		String render = new ConvolutionalNetworkTemplate().render(frame);
+		try {
+			Files.writeString(Path.of(""), render);
+		} catch (IOException e) {
+		}
+		System.out.println();
+	}
 
+	private Frame sectionFrameOf(Node c) {
+		return null;
 	}
 }
